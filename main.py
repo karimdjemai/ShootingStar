5b191af9ad84b0a250a453f3c1cffaec9f7d355c
Karim Djemai <karimdjemai1997@gmail.com>
2018-12-19 16:52:51 +0100

    create modelbuilder class

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 import CSVImporter
+import Modelbuilder
 
 # initialize a tensor from the csv
+tensor = CSVImporter().getCSVasTensor()
 
 # build a rnn model
+model =
 
 # train the model
