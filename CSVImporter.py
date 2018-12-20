40248711e0cb56003847a53683f4d79f35ca8a5f
Julius Schenke <juliusschenke@gmail.com>
2018-12-20 21:19:41 +0100

    da die json Datei nur in meinem Ordner ist und nicht im Git  wird es nicht funktonieren aber naja ... :D

--- CSVImporter.py
+++ CSVImporter.py
@@ -1,7 +1,20 @@
 import tensorflow as tf
+import pandas
+import os
 
-class CSVImporter:
+#dfiles = os.listdir('C:/Users/Juliu/PycharmProjects/git_textGenRNN_GRU_LSTM')
+dfiles = os.listdir()
+
+for file in dfiles:
+   print (file)
+
+#os.listdir()
+
+#class CSVImporter:
 
     # return type should be tensor
-    def getCSVasTensor(self):
-        return
+   # def getCSVasTensor(self):
+dfjson = pandas.read_json("8_seadoggs-vs-breakaway-dust2.json")
+print(dfjson)
+    #    return
+
