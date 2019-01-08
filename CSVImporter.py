47d7ff0c94d92600e7418b6124547137c524e011
Julius Schenke <juliusschenke@gmail.com>
2019-01-08 22:29:51 +0100

    used christophers code to create a csv-file and then printed the head of the json/csv files to compare them

--- CSVImporter.py
+++ CSVImporter.py
@@ -1,20 +1,41 @@
 import tensorflow as tf
 import pandas
 import os
+import json
 
-#dfiles = os.listdir('C:/Users/Juliu/PycharmProjects/git_textGenRNN_GRU_LSTM')
-dfiles = os.listdir()
 
-for file in dfiles:
-   print (file)
+class CSVImporter:
 
-#os.listdir()
 
-#class CSVImporter:
+    def get_csv_as_tensor(self):
+        file_name = "8_seadoggs-vs-breakaway-dust2"
+        from pandas.io.json import json_normalize
 
-    # return type should be tensor
-   # def getCSVasTensor(self):
-dfjson = pandas.read_json("8_seadoggs-vs-breakaway-dust2.json")
-print(dfjson)
-    #    return
+        with open(file_name + ".json") as jsonFile:
+            file = json.load(jsonFile)
 
+        csv_file = json_normalize(file["GameInfo"], meta=["id"], record_path="players", )
+        csv_file.to_csv(file_name + ".csv")
+
+        # os.listdir()
+        # dfiles = os.listdir('C:/Users/Juliu/PycharmProjects/git_LSTM_GRU')
+        d_files = os.listdir()
+
+        for file in d_files:
+            print(file)
+
+    df_json = pandas.read_json("8_seadoggs-vs-breakaway-dust2.json")
+    df_csv = pandas.read_csv("8_seadoggs-vs-breakaway-dust2.csv")    #TODO: take in all (JSON/CSV )files ...
+
+    # printing to console
+
+    # print(dfjson)
+    # this can be used to prints out the head of the table
+    # (can be used to check what the file looks like...)
+    print(df_json.head())
+    print("\n")
+    print(df_csv.head())
+
+
+# return type should be tensor
+# return
