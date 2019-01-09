e3326b2e31dce1359ff934fa9991ad3937429c97
Karim Djemai <karimdjemai1997@gmail.com>
2019-01-09 16:34:54 +0100

    start building the model

--- Modelbuilder.py
+++ Modelbuilder.py
@@ -1,3 +1,13 @@
+import tensorflow as tf
 
 class Modelbuilder:
-    
+
+    def buildModel(self):
+
+
+        model = tf.keras.Sequential([
+            tf.keras.layers.RNN(),
+            tf.keras.layers.dense()
+        ])
+
+        return model
